2b82f0adc4acba1a43016fc108a071260e5829d0
Vasilki <lukanikhin@mail.ru>
2018-07-27 18:17:29 +0300

    Blue push button and LED on board was added. In case of pushed button LED is ligted up.

--- Src/main.c
+++ Src/main.c
@@ -57,8 +57,8 @@ UART_HandleTypeDef huart1;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_USART1_UART_Init(void);
 static void MX_ADC1_Init(void);
+static void MX_USART1_UART_Init(void);
 
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
@@ -98,8 +98,8 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_USART1_UART_Init();
   MX_ADC1_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -246,11 +246,22 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct;
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PA5 */
   GPIO_InitStruct.Pin = GPIO_PIN_5;
@@ -258,6 +269,13 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
--- Src/main_usercode.c
+++ Src/main_usercode.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include "adc.h"
+#include "button_handle.h"
 
 extern UART_HandleTypeDef huart1; /*declared in main.c*/
 extern ADC_HandleTypeDef hadc1; /*declared in main.c*/
@@ -14,15 +15,37 @@ static unsigned int gl_time = 0;
 
 void main_usercode(void)
 {
-  unsigned int loc_adc_val;
+  unsigned int loc_adc_val=0;
   char loc_buff[40];
 
   mu_UpdatePeriod();
-  HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_5);
-  HAL_Delay(300);
+  if(gl_time == 1)
+  {
+    button_SetActiveButtons('C',13);
+  }
+  else
+  {
+    /*nothing to do*/
+  }
+
+
+  /*HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_5);
+  HAL_Delay(300);*/
   mu_TimeTransmit(&huart1,gl_time);
 
-  loc_adc_val = adc_GetValue(&hadc1);
+  //loc_adc_val = adc_GetValue(&hadc1);
+  button_Processing();
+  loc_adc_val = button_GetButtonState('C',13);
+  if(loc_adc_val != 0)
+  {
+    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5,GPIO_PIN_SET);
+  }
+  else
+  {
+    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5,GPIO_PIN_RESET);
+  }
+
+
   sprintf(loc_buff,"ADC VALUE=%d\n\r",loc_adc_val);
   HAL_UART_Transmit(&huart1, (uint8_t*)loc_buff, strlen(loc_buff),0xFFFF);
 
